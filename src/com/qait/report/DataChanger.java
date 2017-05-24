23fa077196e0f119842f6e45aec0177f1c67604a
Saurabh Rai <saurabhrai@QAIT.COM>
2017-05-24 19:57:46 +0530

    working on DataChanger

--- src/com/qait/report/DataChanger.java
+++ src/com/qait/report/DataChanger.java
@@ -4,7 +4,13 @@ public class DataChanger implements AppendData {
 
 	static void dataChanger(String prodLoc){
 		
-		String tempPath = "\\content\\pages";
+		String tempPath = prodLoc + "\\content\\pages";
+//		String overTime = tempPath + "\\OverTime.html";
+//		String responseTimes = tempPath + "\\ResponseTimes.html";
+//		String throughput = tempPath + "\\Throughput.html";
+//		String index = "\\index.html";
+		String[] pathsOfFile = {tempPath + "\\OverTime.html",tempPath + "\\ResponseTimes.html",tempPath + "\\Throughput.html","\\index.html"};
+		
 		
 		
 	}
