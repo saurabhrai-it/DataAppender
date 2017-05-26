6188c12800db0a5f2bbc98fd149aa1bdf28381bc
Saurabh Rai <saurabhrai@QAIT.COM>
2017-05-26 18:54:52 +0530

    Trying RandomAccessFile

--- src/com/qait/report/DataChanger.java
+++ src/com/qait/report/DataChanger.java
@@ -23,30 +23,47 @@ public class DataChanger {
 		String[] pathsOfFile = {tempPath + "\\OverTime.html",tempPath + "\\ResponseTimes.html",tempPath + "\\Throughput.html",prodLoc +"\\index.html"};
 		
 		 // Open reader and writer
-	    FileWriter writer = null;
-	    FileReader fileReader = null;
-	    BufferedReader bufferedReader = null;
+//	    FileWriter writer = null;
+//	    FileReader fileReader = null;
+//	    BufferedReader bufferedReader = null;
+//		
+//		for(int temp=0; temp < pathsOfFile.length;temp++)
+//		{
+//			writer = new FileWriter(pathsOfFile[temp]);
+//			fileReader = new FileReader(pathsOfFile[temp]);
+//			bufferedReader = new BufferedReader(fileReader);
+//			while((s=bufferedReader.readLine())!=null)
+//			{
+//				if(s.contains(oldProdName))
+//					s = s.replace(oldProdName, prodName);
+//				else if(s.contains(oldIcon))
+//					s = s.replace(oldIcon, icon);
+//				else if(s.contains(oldHeadingChange))
+//					s = s.replace(oldHeadingChange, headingChange);
+//				writer.write(s);
+//				bufferedReader.close();
+//			}
+//			fileReader.close();
+//			writer.close();
+//			
+//		}		
 		
+		RandomAccessFile RAF = null;
 		for(int temp=0; temp < pathsOfFile.length;temp++)
 		{
-			writer = new FileWriter(pathsOfFile[temp]);
-			fileReader = new FileReader(pathsOfFile[temp]);
-			bufferedReader = new BufferedReader(fileReader);
-			while((s=bufferedReader.readLine())!=null)
-			{
-				if(s.contains(oldProdName))
-					s = s.replace(oldProdName, prodName);
-				else if(s.contains(oldIcon))
-					s = s.replace(oldIcon, icon);
-				else if(s.contains(oldHeadingChange))
-					s = s.replace(oldHeadingChange, headingChange);
-				writer.write(s);
-				bufferedReader.close();
-			}
-			fileReader.close();
-			writer.close();
-			
-		}		
+			RAF = new RandomAccessFile(pathsOfFile[temp], "rw");
+			while((s=RAF.readLine())!=null)
+				{
+					if(s.contains(oldProdName))
+						s = s.replace(oldProdName, prodName);
+					else if(s.contains(oldIcon))
+						s = s.replace(oldIcon, icon);
+					else if(s.contains(oldHeadingChange))
+						s = s.replace(oldHeadingChange, headingChange);
+					RAF.writeChars(s);
+					RAF.close();
+				}
+		}
 		
 	}
 	
